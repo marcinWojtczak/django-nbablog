d80454a69fe6737131fb8e11013b6131ec75129b
marcinWojtczak <marcinwojtczak.pure@gmail.com>
2022-05-28 22:46:23 +0200

    changed ALLOWED_HOSTS

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nbablog-django.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-nbablog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
